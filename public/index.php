ac0073b6517fd9c0d2ee2e249552841c5f2c527d
imshahidzafar <shahidza619@gmail.com>
2024-07-02 05:53:35 +0500

    latest

--- public/index.php
+++ public/index.php
@@ -15,3 +15,5 @@ require __DIR__.'/../vendor/autoload.php';
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/../bootstrap/app.php')
     ->handleRequest(Request::capture());
+    header('Access-Control-Allow-Origin: *');  
+    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
